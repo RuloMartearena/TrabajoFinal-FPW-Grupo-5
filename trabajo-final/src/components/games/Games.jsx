8d684439f3b8230ef93013804d56849eb9e5c377
Akenajy1209 <112119803+Akenajy1209@users.noreply.github.com>
2022-11-07 11:38:50 -0300

    add component games.jsx - vera sergio esteban

---
+++ trabajo-final/src/components/games/Games.jsx
@@ -0,0 +1,15 @@
+import './Games.css';
+
+const Games = (props) => {
+    return (
+        <article className='games-conteiner'>
+            <div className='games-card'>
+                <a className='games-a' href={props.gameHref} target="_blank" rel="noreferrer">
+                    <img className="games-img" src={props.gameImg} alt="Game"></img>
+                </a>
+            </div>
+        </article>
+    );
+}
+
+export default Games;
