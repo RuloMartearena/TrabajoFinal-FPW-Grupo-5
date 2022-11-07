392704301c688c7ef8d8c7c73d30b6ef0da00fc3
TitiBeguier <beguiercartalibro@gmail.com>
2022-11-07 11:35:13 -0300

    add:  developers.css and jsx/css devconteiner Beguier-ignacio

---
+++ trabajo-final/src/components/developers/DevConteiner.jsx
@@ -0,0 +1,38 @@
+import './DevConteiner.css';
+import './LogoDevConteiner.css';
+import './NavBarDevConteiner.css';
+
+const DevConteiner = (props) => {
+    return (
+        <section className='devconteiner'>
+            <nav className="devconteiner-navbar-conteiner">
+                <div className="devconteiner-navbar-menu">
+                    <ul className='devconteiner-navbar-ul'>
+                        <li className='devconteiner-navbar-li'>
+                            <div className='devconteiner-navbar-logo'>
+                                <h2 className='devconteiner-border'>G5</h2>
+                                <h2 className='devconteiner-waves'>G5</h2>
+                            </div>
+                        </li>
+                        <li className='devconteiner-navbar-li'>
+                            <a className='devconteiner-navbar-a' href="/">
+                                Home
+                            </a>
+                        </li>
+                        <li className='devconteiner-navbar-li'>
+                            <div className='devconteiner-navbar-logo'>
+                                <h2 className='devconteiner-border'>G5</h2>
+                                <h2 className='devconteiner-waves'>G5</h2>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+            <section className='developers-conteiner'>
+                {props.children}
+            </section>
+        </section>
+    )
+}
+
+export default DevConteiner;
